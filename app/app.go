dc28724042afafcba5e193ccabc292613b1b86d1
Daniel <daniel_sousa.p@outlook.com>
2022-08-11 23:09:19 -0300

    feat: add env variables

--- app/app.go
+++ app/app.go
@@ -1,15 +1,28 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/dannielss/banking/domain"
 	"github.com/dannielss/banking/service"
 	"github.com/gorilla/mux"
 )
 
+func sanityCheck() {
+	envVariables := []string{"SERVER_ADDRESS", "SERVER_PORT", "DB_USER", "DB_PASS", "DB_ADDR", "DB_PORT", "DB_NAME"}
+
+	for _, v := range envVariables {
+		if os.Getenv(v) == "" {
+			log.Fatal("Environment variable not defined... ", v)
+		}
+	}
+}
+
 func Start() {
+	sanityCheck()
 	router := mux.NewRouter()
 
 	//wiring
@@ -21,5 +34,7 @@ func Start() {
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 }
