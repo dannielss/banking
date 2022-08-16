9accf12c2a94351132f896fe1ebf4d2fbeef8dd5
Daniel <daniel_sousa.p@outlook.com>
2022-08-15 23:24:44 -0300

    feat: create transaction func

--- domain/account.go
+++ domain/account.go
@@ -5,13 +5,15 @@ import (
 	"github.com/dannielss/banking/errs"
 )
 
+const dbTSLayout = "2006-01-02 15:04:05"
+
 type Account struct {
-	AccountId   string
-	CustomerId  string
-	OpeningDate string
-	AccountType string
-	Amount      float64
-	Status      string
+	AccountId   string  `db:"account_id"`
+	CustomerId  string  `db:"customer_id"`
+	OpeningDate string  `db:"opening_date"`
+	AccountType string  `db:"account_type"`
+	Amount      float64 `db:"amount"`
+	Status      string  `db:"status"`
 }
 
 func (a Account) ToNewAccountResponseDto() dto.NewAccountResponse {
@@ -19,5 +21,21 @@ func (a Account) ToNewAccountResponseDto() dto.NewAccountResponse {
 }
 
 type AccountRepository interface {
-	Save(Account) (*Account, *errs.AppError)
+	Save(account Account) (*Account, *errs.AppError)
+	SaveTransaction(transaction Transaction) (*Transaction, *errs.AppError)
+	FindBy(accountId string) (*Account, *errs.AppError)
+}
+
+func (a Account) CanWithdraw(amount float64) bool {
+	return a.Amount >= amount
+}
+
+func NewAccount(customerId, accountType string, amount float64) Account {
+	return Account{
+		CustomerId:  customerId,
+		OpeningDate: dbTSLayout,
+		AccountType: accountType,
+		Amount:      amount,
+		Status:      "1",
+	}
 }
